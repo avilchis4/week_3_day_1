2f844417241eb4b45b972a655e0e6b706bcb3cfa
Vilchis <avilchis4@cps.k12.il.us>
2024-09-10 09:23:14 -0500

    f string and concatnation

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -21,23 +21,38 @@
 
 
 
-
-
-
-
 # # Review
 # create variables for the following :
 # 1. age
+age = 25 #interger variable
 # 2. name
+name = "John" #string variable
 # 3. song
+song = "happy birthday" #string variable
 # 4. food
+food = "apples" #string variables
 # 5. number
+number = 1000 #integer variables
 
 
 # #now include the variables you just made print in the following...
 
 
 # Once upon a time, there was a [age] old coder named [name].
+#concatnation -- + signs around variables
+print ("Once upon a time, there was a " + str(age) + " old coder named " + name + ".")
+print ("There was a " + str(number) + " as well.")
+# age and number in a sentence
+print ("He is " + str(age) + " and has " + str(number) + ".")
 
+date_of_birth = 2021
+number2 = 123
+number3 = 123.456
+number4 = 123.33
+number5 = 4555
+
+print ("In " + str(date_of_birth) + " there were " + str(number2) + " babies born on September 10th and " + str(number5) + " babies born on September 9th. ")
+print (f"The date of birth is {date_of_birth} and the number is {number2} and the number is {number3} and the number is {number4} and the number is {number5}. ")
 
 # [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
+print (f"{name} like to hum the {song} while coding. It was so annoying that their teammates would throw {food} until {name} would stop singing. ")
